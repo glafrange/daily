284011b0578d042864189836017a339f1dccc15c
Gabriel Lafrange <gabriellafrange@gmail.com>
2018-05-09 19:38:37 -0400

    caesar

---
+++ cipher/caesar.c
@@ -0,0 +1,42 @@
+// A cipher program
+
+#include <ctype.h>
+#include <stdio.h>
+#include <string.h>
+
+
+int main(void)
+{
+  
+  
+  char *input_text = "Hello";
+  
+  int given_key = 1;
+  
+  if(given_key < 0)
+  {
+    return 1;
+  }
+  
+  printf("Plain text : %s\n", input_text);
+  printf("Cipher text : ");
+  
+  
+  for (int i = 0; i < strlen(input_text); i++)
+  {
+    int start = 0;
+    int current_char = 0;
+    
+    if (isalpha(input_text[i]))
+    {
+      start = 'a';
+      current_char = (((input_text[i] - start) + given_key) % 26) + start;
+      printf("%c", current_char);
+    }
+  }
+  
+  printf("\n");
+  
+  return 0;
+  
+}
